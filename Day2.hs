136ee40a47d5a35843cd5dca934d5316ec162a13
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-03-09 00:05:32 +0700

    Add files via upload

---
+++ Day2.hs
@@ -0,0 +1,17 @@
+module Main (main) where
+
+import qualified Data.Time.Clock as DTC
+import qualified Data.Time.ISO8601 as ISO
+import Data.Maybe (fromJust)
+import qualified Data.Map as Map
+
+main :: IO ()
+
+main = do
+    contents <- map fromJust . map ISO.parseISO8601 . lines <$> readFile "input.txt"
+    putStrLn $ analyze contents
+
+analyze  :: [DTC.UTCTime] -> String
+analyze times = (reverse $ tail $ reverse ans) ++ "+00:00"
+                        where acc = filter (\(a, b) -> b >= 4) $ Map.toList $ Map.fromListWith (+) $ zip times (repeat 1)
+                              ans = ISO.formatISO8601 $ fst (acc !! 0)
