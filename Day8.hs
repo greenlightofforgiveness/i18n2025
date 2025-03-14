214a96d079f5af8f13f7e0e8001ed25d0149553d
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-03-15 00:16:59 +0700

    Add files via upload

---
+++ Day8.hs
@@ -0,0 +1,36 @@
+module Main (main) where
+
+import Data.Char (isDigit, toLower, isLetter)
+import Data.List (nub, elem)
+import Data.Maybe (fromJust)
+
+import Main.Utf8 (withUtf8)
+
+-- https://stackoverflow.com/questions/44290218/how-do-you-remove-accents-from-a-string-in-haskell
+
+import qualified Data.Text as T
+import Data.Text.ICU.Char
+import Data.Text.ICU.Normalize
+
+canonicalForm :: String -> String
+canonicalForm s = T.unpack noAccents
+  where
+    noAccents = T.filter (not . property Diacritic) normalizedText
+    normalizedText = normalize NFD (T.pack s)
+
+main :: IO ()
+
+main = withUtf8 $ do
+    contents <- lines <$> readFile "input.txt"
+    putStrLn $ analyze contents 0
+
+analyze  :: [String] -> Int -> String
+analyze [] acc = show acc
+analyze (x : xs) acc = analyze xs acc' where ch_long = length x
+                                             cf = map toLower $ canonicalForm x
+                                             acc' | ch_long >= 4 && ch_long <= 12 &&
+                                                    any isDigit x &&
+                                                    any (`elem` "aeiouауоиэыяюе") (filter isLetter cf) &&
+                                                    any (\x -> not (x `elem` "aeiouауоиэыяюе")) (filter isLetter cf) &&
+                                                    (length $ nub cf) == ch_long = acc + 1
+                                                  | otherwise = acc
