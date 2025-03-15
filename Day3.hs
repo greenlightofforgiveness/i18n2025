5255c38eee4ad83f068673ef70999f41f24bba2d
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-03-15 18:35:38 +0700

    Update Day3.hs

--- Day3.hs
+++ Day3.hs
@@ -15,5 +15,9 @@ main = withUtf8 $ do
 analyze  :: [String] -> Int -> String
 analyze [] acc = show acc
 analyze (x : xs) acc = analyze xs acc' where ch_long = length x
-                                             acc' | ch_long >= 4 && ch_long <= 12 && any isDigit x && any isUpper x && any isLower x && any (not . isAscii) x = acc + 1
+                                             acc' | ch_long >= 4 && ch_long <= 12 &&
+                                                    any isDigit x &&
+                                                    any isUpper x &&
+                                                    any isLower x &&
+                                                    any (not . isAscii) x = acc + 1
                                                   | otherwise = acc
