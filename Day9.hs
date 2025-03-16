ed1c1922559a785b0ebe9a00bce8e351f849e8af
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2025-03-16 11:26:34 +0700

    Update Day9.hs

--- Day9.hs
+++ Day9.hs
@@ -9,7 +9,7 @@ import Main.Utf8 (withUtf8)
 main :: IO ()
 
 main = withUtf8 $ do
-    contents <- lines <$> readFile "input.txt" --  isGood ["11-09-01","09-11-01","31-09-11","16-05-18"] -- 
+    contents <- lines <$> readFile "input.txt"
     putStrLn $ unwords $ Map.keys $ Map.filter isGood $ analyze contents []
 
 analyze  :: [String] -> [(String, [String])] -> Map.Map String [String]
@@ -21,9 +21,10 @@ analyze (x : xs) acc = analyze xs acc'
                                               acc' = acc ++ (zip names (repeat [date]))
                                               
 isGood :: [String] -> Bool
-isGood inp = let x = map (\x -> read x :: Int) $ map (\x -> x !! 0) $ map (splitOn "-") inp
-                 y = map (\x -> read x :: Int) $ map (\x -> x !! 1) $ map (splitOn "-") inp
-                 z = map (\x -> read x :: Int) $ map (\x -> x !! 2) $ map (splitOn "-") inp
+isGood inp = let inp' = map (splitOn "-") inp
+                 x = map (\x -> read x :: Int) $ map (\x -> x !! 0) $ inp'
+                 y = map (\x -> read x :: Int) $ map (\x -> x !! 1) $ inp'
+                 z = map (\x -> read x :: Int) $ map (\x -> x !! 2) $ inp'
                  t1 = if (all (<= 12) x) && (all (<= 31) y) then ["MDY"] else []
                  t2 = if (all (<= 31) x) && (all (<= 12) y) then ["DMY"] else []
                  t3 = if (all (<= 31) y) && (all (<= 12) z) then ["YDM"] else []
